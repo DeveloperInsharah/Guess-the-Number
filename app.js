1a23e62f2a07f05628c76f815007ad16402a99c7
Insharah's Developer <insharahattariya@gmail.com>
2024-11-20 08:32:18 -0800

    Game Done

---
+++ app.js
@@ -0,0 +1,4 @@
+let gameNum=45;
+let userNum=prompt("Hey! Guess the Game Number")
+while(userNum!=gameNum){userNum=prompt("Your number is wrong . Guess again.")}
+console.log("Congratulations! 🎉 You can win 🥳🎊🎁.")
